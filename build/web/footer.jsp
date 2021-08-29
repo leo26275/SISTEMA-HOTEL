c4a49e714789e0300941b391314caba1a78234d9
Leo Garcia <leo26275@gmail.com>
2021-08-29 08:29:37 +0200

    Finished project

---
+++ build/web/footer.jsp
@@ -0,0 +1,9 @@
+<%-- 
+    Document   : footer
+    Created on : 14/04/2019, 05:24:30 PM
+    Author     : Sony
+--%>
+
+<footer class="main-footer">
+    <strong>Copyright &copy; 2019 </strong> UES
+</footer>
---
+++ src/java/Adicionales/Totales.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Adicionales;
+
+import modelo.dao.DaoConsumo;
+import modelo.entidad.Consumo;
+
+/**
+ *
+ * @author Sony
+ */
+public class Totales {
+
+    DaoConsumo daoconsumo = new DaoConsumo();
+
+    public Totales() {
+    }
+
+    public double totalconsumido() {
+        double tot = 0;
+
+        for (Consumo c : daoconsumo.ObtenerlistaDeConsumosEstado()) {
+            tot += c.total();
+        }
+
+        return tot;
+    }
+}
